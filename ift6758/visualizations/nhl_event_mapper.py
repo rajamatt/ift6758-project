95cfee7672852217d949e16bd7bf4afa6b638054
Matthew Rajala <matthew.rajala@polymtl.ca>
2024-10-13 16:15:00 -0400

    refactoring debugger

--- ift6758/visualizations/nhl_event_mapper.py
+++ ift6758/visualizations/nhl_event_mapper.py
@@ -13,9 +13,14 @@ class NHLEventMapper:
         self.helper = NHLHelper()
         self.local_data_path = os.getenv('RINK_IMG_PATH')
         self.rink_image_path = os.path.join(self.local_data_path, f'nhl_rink.png')
+        self.output_area = widgets.Output()
 
 
-    def event_summary(self,plays:dict,n_event:int,home_team:'str',away_team:'str'):
+    def get_output_area(self):
+        return self.output_area
+
+
+    def event_summary(self,plays:dict,n_event:int,home_team:str,away_team:str):
         """Displays play/event specific information for a specific game
 
         Args:
@@ -24,14 +29,14 @@ class NHLEventMapper:
             plays (dict): The play by play data from the JSON file as a dictonary
             n_event (int): The event id of the play/event being inspected
         """
-        #Extract Info about the play
+        # Extract Info about the play
         play = plays[n_event-1]
         play_type = play['typeDescKey']
         play_period =play['periodDescriptor']['number']
         play_period_type = play['periodDescriptor']['periodType']
         play_period_time = play['timeInPeriod']
         
-        #create layout for widgets and display the info
+        # Create layout for widgets and display the info
         grid = widgets.GridspecLayout(2,4,grid_gap='0',width='25%',align_items='center')
         grid[0,0]= widgets.HTML(value='Play Type')
         grid[0,1]= widgets.HTML(value='Period')
@@ -41,7 +46,9 @@ class NHLEventMapper:
         grid[1,1]= widgets.HTML(value= str(play_period))
         grid[1,2]= widgets.HTML(value=play_period_type )
         grid[1,3]= widgets.HTML(value =play_period_time)
-        display(grid)
+        
+        with self.output_area:
+            display(grid)
         
         # Pre 2019 the JSON Files did not specify which side the teams where on for each play
         if 'homeTeamDefendingSide' in play.keys():
@@ -64,54 +71,108 @@ class NHLEventMapper:
             if 'xCoord' in play['details']:
                 x = play['details']['xCoord']
                 y = play['details']['yCoord']
-                plt.plot(x,y, marker='o', color="purple",markersize = 10) 
+                plt.plot(x,y, marker='o', color="purple",markersize = 10)
+
+        with self.output_area:
+            plt.show()
         
     
-    def summary(self,season:int,type:str,n_game:int):
-        """Displays a summary of the game information
+    def summary(self, season: int, season_type: str, n_game: int, round_num: int = 1, matchup: int = 1):
+        """Displays a summary of the game information.
 
         Args:
             season (int): The season of the NHL
-            type (str): 'regular' or 'playoff' 
-            n_game (int): game number (max possible: 1312)
+            season_type (str): 'Regular season' or 'Playoffs' 
+            n_game (int): Game number.
+            round_num (int): Round of playoffs. Defaults to None.
+            matchup (int): Matchup of playoffs. Defaults to None.
         """
-        #Create gameid from given info
         game_id = None
 
-        if type == 'regular':
+        if season_type == 'Regular season':
             game_id = self.helper.construct_regular_season_game_id(season, n_game)
         else:
-            display('under construction')
+            # Check if round and matchup are provided for playoffs
+            if round_num is None or matchup is None:
+                raise ValueError("Both 'round_num' and 'matchup' must be provided for playoffs.")
+            
+            game_id = self.helper.construct_playoff_season_game_id(season, round_num, matchup, n_game)
 
-        #Fetch Data from API if it doesnt exist
         if not self.data_fetcher.game_already_fetched(game_id):
             self.data_fetcher.fetch_raw_game_data(game_id)
 
-        #Load the JSON file into python
-        game_data = []
         with open(self.data_fetcher.get_game_local_path(game_id), 'r') as file:
             game_data = json.load(file)
 
-        #Extract Game Info
+        # Extract and display game information
         venue = game_data['venue']['default']
         home_team = game_data['homeTeam']['abbrev']
         away_team = game_data['awayTeam']['abbrev']
         home_score = game_data['homeTeam']['score']
         away_score = game_data['awayTeam']['score']
-        
-        #create layout for widgets and display the info
-        display(widgets.HTML(value=venue,description='Venue:'))
-        grid = widgets.GridspecLayout(3,3,grid_gap='0',width='25%',align_items='center')
-        grid[0,1]= widgets.HTML(value="Home")
-        grid[0,2]= widgets.HTML(value="Away")
-        grid[1,1]= widgets.HTML(value=home_team)
-        grid[1,2]= widgets.HTML(value=away_team)
-        grid[2,0]= widgets.HTML(value='SCORE')
-        grid[2,1]= widgets.HTML(value=str(home_score))
-        grid[2,2]= widgets.HTML(value=str(away_score))
-        display(grid)
 
-        #Extract play by play data and run a widget over the max number of plays =length of play by play data
+        with self.output_area:
+            display(widgets.HTML(value=f"Venue: {venue}"))
+
+        grid = widgets.GridspecLayout(3, 3, grid_gap='0', width='25%', align_items='center')
+        grid[0, 1] = widgets.HTML(value="Home")
+        grid[0, 2] = widgets.HTML(value="Away")
+        grid[1, 1] = widgets.HTML(value=home_team)
+        grid[1, 2] = widgets.HTML(value=away_team)
+        grid[2, 0] = widgets.HTML(value='SCORE')
+        grid[2, 1] = widgets.HTML(value=str(home_score))
+        grid[2, 2] = widgets.HTML(value=str(away_score))
+        
+        with self.output_area:
+            display(grid)
+
+        # Event summary interaction
         plays_data = game_data['plays']
-        widgets.interact(self.event_summary,plays=widgets.fixed(plays_data),n_event= (1,len(plays_data),1),home_team= widgets.fixed(home_team) , away_team = widgets.fixed(away_team))
- 
+        w = widgets.interactive(
+            self.event_summary,
+            plays=widgets.fixed(plays_data),
+            n_event=(1, len(plays_data), 1),
+            home_team=widgets.fixed(home_team),
+            away_team=widgets.fixed(away_team)
+        )
+
+        with self.output_area:
+            display(w)
+
+
+    def create_widgets(self, season_type: str):
+        if season_type == 'Playoffs':
+            return {
+                'round_num': widgets.IntSlider(min=1, max=4, description='Round'),
+                'matchup': widgets.IntSlider(min=1, max=8, description='Matchup'),
+                'n_game': widgets.IntSlider(min=1, max=7, description='Game')
+            }
+        else:
+            return {
+                'n_game': widgets.IntSlider(min=1, max=1312, description='Game')
+            }
+
+
+    def update_widgets(self, season_type: str, season: int):
+        self.output_area.clear_output(True)
+        widget_dict = self.create_widgets(season_type)
+
+        if season_type == 'Regular season':
+            w = widgets.interactive(
+                self.summary,
+                season=widgets.fixed(season),
+                season_type=widgets.fixed(season_type),
+                n_game=widget_dict['n_game']
+            )
+        else:
+            w = widgets.interactive(
+                self.summary,
+                season=widgets.fixed(season),
+                season_type=widgets.fixed(season_type),
+                round_num=widget_dict['round_num'],
+                matchup=widget_dict['matchup'],
+                n_game=widget_dict['n_game']
+            )
+
+        with self.output_area:
+            display(w)
